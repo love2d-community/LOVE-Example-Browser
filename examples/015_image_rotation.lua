63f8bf4f70f2ff42a5b4732e5a66b8634a46a40b
Megadardery <Megadardery@yahoo.com>
2016-02-12 15:02:09 +0200

    Updating example 15 with the suggestions of tuupakku.

--- examples/015_image_rotation.lua
+++ examples/015_image_rotation.lua
@@ -7,12 +7,10 @@ function love.load()
 end
 
 function love.update(dt)
-    angle = angle + dt
+    angle = (angle + dt) % (2 * math.pi)
     x, y = 400 + math.cos(angle)*100, 300 + math.sin(angle)*100
 end
 
 function love.draw()
-    love.graphics.draw(image, x, y)
+    love.graphics.draw(image, x, y,angle)
 end
-
-
